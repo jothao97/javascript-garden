0aee39a487c2496b1842742ede04eda1496b5528
JoAnn <joann.thao@live.com>
2024-03-05 16:02:35 -0600

    almost done 11

--- assignment/scripts/garden.js
+++ assignment/scripts/garden.js
@@ -9,30 +9,40 @@ var plantHeight = 18
 var plantLocation = 'balcony'
 
 // 4 - Console log the value of each variable
-console.log (plantType)
+console.log (plantType, plantHeight, plantLocation)
 
 // 5 - Create a variable named `isOutdoor` and set it to a boolean value (true or false) indicating whether the plant is outdoors
-
+var isOutdoor= true
 
 // 6 - Create a variable named `waterFrequency` and set its value to a string describing how often the plant needs watering (e.g., "Daily", "Weekly", "Bi-weekly")
-
+var waterFrequency = 'weekly'
 
 // 7 - Create a variable called `daysSinceLastWatering` and set it to the number of days since the plant was last watered
-
+var daysSinceLastWatering = 7
 
 // 8 - Create a conditional: if `isOutdoor` is true, console log "Remember to check the weather forecast!"
-
+var isOutdoor
+    if (isOutdoor = true) {
+    console.log("Remember to check the weather forecast!")
+}
 
 // 9 - Write a conditional that console logs "Water the plant" if the value of `daysSinceLastWatering` is greater than or equal to 2,
 // and console logs "Do not water the plant yet" if the value is less than 2.
-
+var daysSinceLastWatering
+    if (daysSinceLastWatering >= 2) {
+        console.log('water the plant');
+    } else {
+        console.log('do not water the plant yet')
+    }
 
 // 10 - Create a variable called `daysToWater` and set it to 7
+var daystoWater = 7
 
 
 // 11 - Create a while loop that logs "Water the plant" while `daysToWater` is greater than 0,
 // and decrements `daysToWater` by 1 in each iteration
-
+var i=0;
+while (i>0; i++)
 
 // 12 - Create a for loop that logs "Check for pests" for each day of the week (i.e., 7 times)
 
